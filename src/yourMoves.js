6e0b41b59a7aae8585b59c3675a55915a8eb6399
Purvi Kanal <purvikanal92@gmail.com>
2019-01-04 16:43:50 -0500

    ⭕️ Purvis move

--- src/yourMoves.js
+++ src/yourMoves.js
@@ -1,6 +1,6 @@
 export const x = "❌";
 export const o = "⭕️";
 
-const moves = [[[o], [], []], [[x], [x], [o]], [[], [x], []]];
+const moves = [[[o], [o], []], [[x], [x], [o]], [[], [x], []]];
 
 export default moves;
